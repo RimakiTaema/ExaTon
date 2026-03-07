f357733251f3e8f96b1721caabc2f957ab452abf
RimakiTaema <txwfyxa@gmail.com>
2026-03-07 23:21:05 +0700

    big update 1

--- lib/exaroton/src/apis/accounts.dart
+++ lib/exaroton/src/apis/accounts.dart
@@ -1,19 +1,16 @@
-import 'package:dio/dio.dart';
-import 'package:openapi/openapi.dart';
+import 'package:openapi_client/api.dart';
 
 class GetAccount {
   final String token;
 
   GetAccount({required this.token});
 
-  Future<Response<GetAccount200Response>> run() async {
-    final dio = Dio();
+  Future<GetAccount200Response> run() async {
+    final apiClient = ApiClient(basePath: 'https://api.exaroton.com/v1');
+    apiClient.addDefaultHeader('Authorization', 'Bearer $token');
 
-    dio.options.headers["Authorization"] = "Bearer $token";
-
-    final apiClient = AccountApi(dio, standardSerializers);
-
-    final res = await apiClient.getAccount();
+    final accountApi = AccountApi(apiClient);
+    final res = await accountApi.getAccount();
 
     return res;
   }
