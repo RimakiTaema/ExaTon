fe7b13e7280297e126896c2194da90b358fec574
RimakiTaema <txwfyxa@gmail.com>
2026-04-19 17:19:27 +0700

    chore: Bumped To 1.1.0

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
+import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
   output: "export",
-  distDir: "out"
+  distDir: "out",
+  outputFileTracingRoot: path.resolve(import.meta.dirname),
+  turbopack: {
+    root: path.resolve(import.meta.dirname),
+  },
 };
 
 export default nextConfig;
